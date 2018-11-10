c028c09f5de52b75344ddb556cb71335e9c3fe5b
Nick Lima <nlima446@gmail.com>
2018-11-09 22:37:49 -0500

    working with node/express, testing push

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/bundle.js', function(req, res) {
+    res.sendFile(path.join(__dirname + '/bundle.js'));
+});
+app.get('/data.json', function(req, res) {
+    res.sendFile(path.join(__dirname + '/data.json'));
+});
+
+app.get('/styles.css', function(req, res) {
+    res.sendFile(path.join(__dirname + '/styles.css'));
+});
+
+app.listen(3000);
---
+++ test.js
@@ -0,0 +1,1 @@
+function sayHello() { alert("Hello world") }
